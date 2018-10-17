ba003439061e34980a9479da016f4423abbd1f65
Wojtek M <wojciech.moczydlowski98@gmail.com>
2018-10-17 23:53:38 +0200

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styles from "./about.module.css"
+import Container from "../components/container.js"
+
+console.log(styles);
+export default () => (
+  <Container>
+    <h1>About CSS Modules</h1>
+    <p>CSS Modules are cool</p>
+  </Container>
+)
